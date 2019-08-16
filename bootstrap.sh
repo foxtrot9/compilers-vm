c4cc8a80e77efa6250b13df627b18a4c231fa586
Mit Naria <mit4dev@gmail.com>
2019-08-16 12:32:48 +0530

    Fix link for resources binaries and path.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,10 @@ sudo apt-get install -y flex bison build-essential csh openjdk-6-jdk libxaw7-dev
 sudo mkdir /usr/class
 sudo chown vagrant /usr/class
 cd /usr/class
-wget http://spark-university.s3.amazonaws.com/stanford-compilers/vm/student-dist.tar.gz
+mkdir cs143
+mkdir cs143/cool/
+cd /usr/class/cs143/cool/
+wget https://s3-us-west-1.amazonaws.com/prod-edx/Compilers/Misc/student-dist.tar.gz
 tar -xf student-dist.tar.gz
 ln -s /usr/class/cs143/cool /home/vagrant
 echo "export PATH=/usr/class/cs143/cool/bin:\$PATH" >> /home/vagrant/.bashrc
